3dc202deeffcb404bed9655055c04c0ed51266ca
Steven E. Pav <shabbychef@gmail.com>
2015-02-24 09:31:19 -0800

    dnt description

---
+++ man-roxygen/desc-dnt.R
@@ -0,0 +1,10 @@
+#' The doubly noncentral t distribution generalizes the (singly)
+#' noncentral t distribution to the case where the numerator is
+#' the square root of a scaled noncentral chi-square distribution.
+#' That is, if 
+#' \eqn{X \sim \mathcal{N}\left(\mu,1\right)}{X ~ N(u,1)} independently
+#' of \eqn{Y \sim \chi^2\left(k,\theta\right)}{Y ~ x^2(k,theta)}, then
+#' the random variable
+#' \deqn{T = \frac{X}{\sqrt{Y/k}}}{T = X / sqrt(Y/k)}
+#' takes a doubly non-central t distribution with parameters
+#' \eqn{k, \mu, \theta}{k, mu, theta}.
