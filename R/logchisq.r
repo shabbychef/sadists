5f703bc236b3117fd0221c8257e4d2a237183676
Steven Pav <shabbychef@gmail.com>
2015-03-18 20:45:32 -0700

    got the moments down

--- R/logchisq.r
+++ R/logchisq.r
@@ -7,7 +7,7 @@
 
 # cumulants of the log of the central chi-square
 lc_cumulants <- function(df,order.max=3,orders=c(1:order.max)) {
-	kappa <- psigamma(df/2,deriv=orders)
+	kappa <- psigamma(df/2,deriv=orders-1)
 	kappa[1] <- kappa[1] + log(2)
 	return(kappa)
 }
@@ -17,13 +17,19 @@ lc_moments <- function(df,order.max=3,orders=c(1:order.max)) {
 	mu <- PDQutils::cumulant2moment(kappa)
 	return(mu)
 }
-# that not right ...
 # moments of the log of the non-central chi-square
 lnc_moments <- function(df,ncp=0,order.max=3,orders=c(1:order.max)) {
-	allmu <- sapply(0:50,function(iv) {
-									(exp(-ncp/2) * ((ncp/2)**(iv)) / factorial(iv)) * lc_moments(df+2*iv,orders=orders)
-		},simplify=FALSE)
-	mu <- Reduce('+',allmu)
+	stopifnot(ncp >=0)
+	if (ncp > 0) {
+		hancp <- ncp / 2.0
+		# should be smarter about 0:50 here.
+		allmu <- sapply(0:50,function(iv) {
+										exp(-hancp + iv * log(hancp) - lfactorial(iv)) * lc_moments(df+2*iv,orders=orders)
+			},simplify=FALSE)
+		mu <- Reduce('+',allmu)
+	} else {
+		mu <- lc_moments(df=df,orders=orders)
+	}
 	return(mu)
 }
 # cumulants of the log of the non-central chi-square
@@ -32,6 +38,7 @@ lnc_cumulants <- function(df,ncp=0,order.max=3,orders=c(1:order.max)) {
 	kappa <- PDQutils::moment2cumulant(mu)
 	return(kappa)
 }
+# now proceed as usual! log noncentral chisquare!
 
 
 
